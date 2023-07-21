41c22cf0c42c81841a30eb7e839c6b037289b591
MKiranAchari <138939408+MKiranAchari@users.noreply.github.com>
2023-07-21 22:28:35 +0530

    Update sample1.js

--- javascript/sample1.js
+++ javascript/sample1.js
@@ -1,6 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 const myCarouselElement = document.querySelector('#myCarousel')
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyoQPDaaR1YTUFjkU7weJ_Y9uCTE7hoOK_5yUQVjE8EjVuEIRBh/exec'
+const form = document.forms['submit-to-google-sheet']
 
 let observer = new IntersectionObserver((entries, observer) => {
     entries.filter(e => e.isIntersecting).forEach(entry => {
@@ -15,3 +17,9 @@ document.querySelectorAll('section')
 function sendfeed(){
     alert("save the data in the system")
 }
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
